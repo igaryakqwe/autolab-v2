4aa12d2b4c29177b0bf0fd380bab9f6b5b0a5660
igaryakqwe <igor.novykov@gmail.com>
2025-07-19 16:46:34 +0300

    chore: add prefetching to trpc

--- src/lib/trpc/server.ts
+++ src/lib/trpc/server.ts
@@ -3,12 +3,9 @@ import 'server-only';
 import { cache } from 'react';
 import { headers } from 'next/headers';
 import { createTRPCContext } from '@/server/api/trpc';
-import { appRouter, AppRouter } from '@/server/api/root';
-import { createTRPCClient, httpLink, TRPCClientError } from '@trpc/client';
-import { getUrl, transformer } from '@/lib/trpc/shared';
-import { observable } from '@trpc/server/observable';
-import { callProcedure } from '@trpc/server';
-import { TRPCErrorResponse } from '@trpc/server/rpc';
+import { createHydrationHelpers } from '@trpc/react-query/rsc';
+import { AppRouter, createCaller } from '@/server/api/root';
+import { createQueryClient } from '@/lib/trpc/shared';
 
 const createContext = cache(async () => {
   const heads = new Headers(headers());
@@ -19,38 +16,10 @@ const createContext = cache(async () => {
   });
 });
 
-export const trpc = createTRPCClient<AppRouter>({
-  links: [
-    httpLink({
-      url: getUrl(),
-      transformer,
-    }),
-    /**
-     * Custom RSC link that lets us invoke procedures without using http requests. Since Server
-     * Components always run on the server, we can just call the procedure as a function.
-     */
-    () =>
-      ({ op }) =>
-        observable((observer) => {
-          createContext()
-            .then((ctx) => {
-              return callProcedure({
-                procedures: appRouter._def.procedures,
-                path: op.path,
-                input: op.input,
-                signal: op.signal ?? undefined,
-                getRawInput: async () => op.input,
-                ctx,
-                type: op.type,
-              });
-            })
-            .then((data) => {
-              observer.next({ result: { data } });
-              observer.complete();
-            })
-            .catch((cause: TRPCErrorResponse) => {
-              observer.error(TRPCClientError.from(cause));
-            });
-        }),
-  ],
-});
+const getQueryClient = cache(createQueryClient);
+const caller = createCaller(createContext);
+
+export const { trpc, HydrateClient } = createHydrationHelpers<AppRouter>(
+  caller,
+  getQueryClient,
+);
